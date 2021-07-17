09fde5a6f45eece9741a53fb2c84bbd2ecb72ccc
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2021-07-17 19:32:51 +0530

    changes

--- packages/web3-utils/types/index.d.ts
+++ packages/web3-utils/types/index.d.ts
@@ -1,7 +1,7 @@
 /*
     This file is part of xdc3.js.
 
-    web3.js is free software: you can redistribute it and/or modify
+    xdc3.js is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
